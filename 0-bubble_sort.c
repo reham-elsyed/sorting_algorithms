e121b22e8f3efc30d00f216b5ae5a2f8dda5ec8a
reham-elsyed <rehamshepl666@gmail.com>
2024-02-15 13:11:21 -0800

    p

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -28,13 +28,10 @@ void bubble_sort(int *array, size_t size)
 				temp = array[b];
 				array[b] = array[b + 1];
 				array[b + 1] = temp;
-
-			
-		
 				print_array(array, size);
 				flag = 0;
-		}}	
+			}
+		}
 	}
 }
 }
-
