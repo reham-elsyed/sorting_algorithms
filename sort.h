a93ac16481a07bf5161964e31bcb24a8e855050e
reham-elsyed <rehamshepl666@gmail.com>
2024-02-16 14:40:08 -0800

    k

--- sort.h
+++ sort.h
@@ -25,4 +25,5 @@ void print_array(const int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2);
 void quick_sort_hoare(int *array, size_t size);
+void selection_sort(int *array, size_t size);
 #endif
