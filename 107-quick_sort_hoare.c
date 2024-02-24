309ce4e751f50c2756a739f17d37301cce37e604
reham-elsyed <rehamshepl666@gmail.com>
2024-02-24 07:04:21 -0800

    quick hoare

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -1,50 +1,69 @@
 #include "sort.h"
-
-
 /**
- *swap_counter- function to swap
- *@array:input array
- *@i: input int 1
- *@j: input int 2
+ *swap-functio nto psawp
+ *@a: pointer to element
+ *@b: pointer to element
  *Return: void
  */
+void swap(int *a, int *b){
+  int temp;
 
-void swap_counter(int *array,int i, int j)
-{
-	int temp;
-
-	  temp = array[i];
-	  array[i] = array[j];
-	  array[j] = temp;
+  temp = *a;
+  *a = *b;
+  *b = temp;
 }
 /**
- *quick_sort_hoare - function to sort
+ *_partition - function to partition of hoare partition
  *@array: input array
- *@size:input size
+ *@size: size
+ *@start: start
+ *@end: end point
+ *Return: void
+ */
+int _partition(int *array,size_t size, size_t left, size_t right){
+  int i, j, pivot;
+  pivot = array[right];
+ for (i = left - 1, j = right + 1; i < j;)
+   {
+  do{
+    i++;
+  }while (array[i] < pivot);
+  do{
+    j--;
+  } while (array[j] > pivot);
+     if (i < j)
+     {
+    swap(&array[i], &array[j]);
+     print_array(array, size);
+     }}
+  return (i);
+}
+/**
+ *Quick_recursive- function to recursive input array atarting and
+ *@array: input array
+ *@size: size
+ *@start: start
+ *@end: end point
+ *Return: void
+ */
+void quick_recursive(int *array, size_t size, int start, int end)
+{
+ int partition;
+  if (end - start > 0)
+  {
+   partition = _partition(array, size, start, end);
+    quick_recursive(array, size, start, partition - 1);
+    quick_recursive(array, size, partition, end);
+}}
+/**
+ *quick_sort -function to sort quicksort
+ *@array: input array of intgers
+ *@size: input sizr of arrsy
  *Return: void
  */
 void quick_sort_hoare(int *array, size_t size)
 {
-	int i, j, pivot;
-
-	if (size < 2)
-		return;
-
-	pivot = array[size / 2];
-	for (i = 0; i < pivot - 1; i++)
-	{
-		for (j = size - 1; j > 0; j--)
-		{
-			if (array[i] > pivot || array[j] < pivot)
-			{
-		if (i <= j)
-		{
-			swap_counter(array, i, j);
-		
-		}
-
-			print_array(array, size);
-		}}}
-	quick_sort_hoare(array, j + 1);
-	quick_sort_hoare(array + i, size - 1);
+  if (array == NULL || size < 2)
+    return;
+  quick_recursive(array,size, 0, size - 1);
 }
